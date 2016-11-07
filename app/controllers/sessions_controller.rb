7997e4fc9af231ab28c7053e952eb78033f80489
Robert Turner Jr <georob23@gmail.com>
2016-11-07 08:20:12 -0800

    Update styling. Add tabs. Update schema plan.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,30 @@
 #NEW
 
 get '/sessions/login/?' do
-   erb :'sessions/_login.html', layout: !request.xhr?
+  erb :'sessions/_login.html', layout: !request.xhr?
 end
 
 #CREATE
 
 post '/sessions/login/?' do
 
-   @user = User.find_by_username(params[:user][:email])
+  @user = User.find_by(email: params[:user][:email])
 
-   if @user && @user.password == params[:user][:password]
-      session[:id] = @user.id
-      
-      erb :'/headers/_header_links.html', layout: !request.xhr?
-   else
-      @errors = "Email & Password not found"
-      erb :'/sessions/_login.html', layout: !request.xhr?
-   end
+  if @user && @user.password == params[:user][:password]
+    session[:id] = @user.id
 
+    erb :'/index'
+    # erb :'/headers/_header_links.html', layout: !request.xhr?
+  else
+    @errors = "Email & Password not found"
+    erb :'/sessions/_login.html', layout: !request.xhr?
+  end
 end
 
 # DELETE
 
 delete '/sessions/logout/?' do
-   session[:id] = nil
-   erb :'/headers/_header_links.html', layout: !request.xhr?
+  session[:id] = nil
+  erb :'/index'
+  #  erb :'/headers/_header_links.html', layout: !request.xhr?
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,27 @@
-$( document ).ready( function () {
-  
-};
+$(document).ready(function () {
+  displayDefault();
+  tabListener();
+  // event.preventDefault();
+
+  // Hide all of the tabs and then show ONLY the one that you clicked on
+  // $(".tab-content").hide();
+  // $tab_id.show();
+});
+
+var displayDefault = function() {
+  // Find and display content of first (default) tab
+  var start = $("li").first().children().first().attr("href");
+  $( start ).show();
+}
+
+var tabListener = function() {
+  // Get tab id that is clicked on and make that <li> active
+  $( ".tabs" ).click(function( event ) {
+    event.preventDefault();
+    $(event.target).parent().addClass("active").siblings().removeClass("active");
+
+    // AJAX CALLS SHOULD GO HERE!!!
+    tab_id = $(event.target).attr("href");
+    $tab_id = $( tab_id );
+  });
+}
